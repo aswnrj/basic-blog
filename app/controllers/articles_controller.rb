e2061fce37e4999aefbf2a8cce96f545fffc5847
Aswin Raj R <aswnrj.r@gmail.com>
2021-12-12 12:08:16 +0530

    Update edit operation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,26 +12,21 @@ class ArticlesController < ApplicationController
   end
 
   def edit 
-    # render plain: params
     @article = Article.find(params[:id])
   end
 
   def update
-    edited_article = params[:article]
     @article = Article.find(params[:id])
-    @article[:title] = edited_article[:title]
-    @article[:description] = edited_article[:description]
-    @article.save
-    redirect_to articles_path
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    if @article.valid? 
-      @message = "Article saved succesfully"
-    else 
-      @message = "Not saved"
-    end
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to article_path(@article)
